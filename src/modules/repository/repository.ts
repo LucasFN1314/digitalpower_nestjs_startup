379b7c5776610bbc17cd829dd33349e09ce6e498
Your Name <you@example.com>
2024-08-01 12:53:38 -0300

    Repository Service Model

--- src/modules/repository/repository.ts
+++ src/modules/repository/repository.ts
@@ -1,4 +1,6 @@
-import { HttpException, HttpStatus } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 export async function RepositoryAction(service: any, action: any, payload: any) {
     return await service.repository?.[action](payload);
@@ -18,5 +20,33 @@ export async function Delete(service: any, id: number) {
 export async function ResponseMessage(message: any) {
     return {
         message
+    }
+}
+
+@Injectable()
+export class RepositoryService {
+    async create(dto: any) {
+        return await SaveModel(this, dto);
+    }
+
+    async findAll(arg?: any) {
+        return await RepositoryAction(this, "find", arg);
+    }
+
+    async findOne(id: number) {
+        return await RepositoryAction(this, "findOneBy", { id });
+    }
+
+    async findSelect(fields: any) {
+        return await RepositoryAction(this, "find", fields);
+    }
+
+    async update(id: number, dto: any) {
+
+    }
+
+    async remove(id: number) {
+        await Delete(this, id);
+        return ResponseMessage("Elemento eliminado");
     }
 }
