2721f16e33e5024db3c066c08d4ad1b61cbfe79f
lmartz <lucas@digitalpower.ar>
2024-08-16 15:16:50 -0300

    Change Auth Middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,16 +1,26 @@
-import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { NextFunction, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(private readonly configService: ConfigService) {
-  }
+  constructor(private readonly configService: ConfigService) {}
 
   async use(req: any, res: Response, next: NextFunction) {
     const secret = this.configService.get<string>('SECRET_KEY');
-    const token = req.headers.authorization.split(' ')[1];
+    let token = '';
+    try {
+      token = req.headers.authorization.split(' ')[1];
+    } catch (error) {
+      throw new HttpException('Token no provisto', HttpStatus.UNAUTHORIZED);
+    }
+
     try {
       req.user = await jwt.verify(token, secret);
     } catch (error) {
